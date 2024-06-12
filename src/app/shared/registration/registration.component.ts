389bf9d1ce2f7d096c5e9c99491dc6544ed91b36
Blen <blenassefa59@yahoo.com>
2024-06-12 18:53:07 +0300

    blen.fix: build error fix

--- src/app/shared/registration/registration.component.ts
+++ src/app/shared/registration/registration.component.ts
@@ -17,11 +17,11 @@ export class RegistrationComponent {
   isIframeLoading: boolean = true;
   sanitizedUrl: SafeResourceUrl | undefined;
   showShareModal = false;
-  telegramSharingLink= "https://t.me/share/url?url=https://res.cloudinary.com/eskalate/image/upload/v1717588776/gallery/Hackathon_Attendance.png&text=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
-  linkedinSharingLink= "https://www.linkedin.com/sharing/share-offsite/?url=https://res.cloudinary.com/eskalate/image/upload/v1717588776/gallery/Hackathon_Attendance.png&summary=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
-  twitterSharingLink= "https://twitter.com/intent/tweet?url=https://pbs.twimg.com/media/GPsyu1fXEAEDXNf?format=jpg&name=medium&text=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20registration!%20https://hackathon.a2sv.org"
-  facebookSharingLink= "https://www.facebook.com/sharer/sharer.php?u=https://res.cloudinary.com/eskalate/image/upload/v1717588776/gallery/Hackathon_Attendance.png&t=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
-  instagramSharingLink = "https://www.instagram.com/?url=https://res.cloudinary.com/eskalate/image/upload/v1717588776/gallery/Hackathon_Attendance.png&text=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
+  telegramSharingLink= "https://t.me/share/url?url=https://res.cloudinary.com/eskalate/image/upload/v1718181258/Hackathon-2024/Hackathon_Registration_Card.png&text=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
+  linkedinSharingLink= "https://www.linkedin.com/sharing/share-offsite/?url=https://res.cloudinary.com/eskalate/image/upload/v1718181258/Hackathon-2024/Hackathon_Registration_Card.png&summary=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
+  twitterSharingLink= "https://twitter.com/intent/tweet?url=https://res.cloudinary.com/eskalate/image/upload/v1718181258/Hackathon-2024/Hackathon_Registration_Card.png&text=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
+  facebookSharingLink= "https://www.facebook.com/sharer/sharer.php?u=https://res.cloudinary.com/eskalate/image/upload/v1718181258/Hackathon-2024/Hackathon_Registration_Card.png&t=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
+  instagramSharingLink = "https://www.instagram.com/?url=https://res.cloudinary.com/eskalate/image/upload/v1718181258/Hackathon-2024/Hackathon_Registration_Card.png&text=I've%20just%20registered%20for%20the%20A2SV%20Hackathon!%20Join%20me%20and%20share%20your%20own%20registration!%20https://hackathon.a2sv.org"
   
   ngOnChanges() {
     this.sanitizedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.registrationLink);
