39d665fff0653fb8822747d21e30ec4c15c1b377
Blen <blenassefa59@yahoo.com>
2024-09-16 14:06:16 +0300

    Blen.update: page addition and improvement

--- src/app/pages/be-asponsor/be-asponsor.component.ts
+++ src/app/pages/be-asponsor/be-asponsor.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Component, Input, ViewChild, ElementRef } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 @Component({
   selector: 'app-be-asponsor',
   // standalone: true,
@@ -10,4 +10,37 @@ import { CommonModule } from '@angular/common';
 })
 export class BeASponsorComponent {
 
+
+  @ViewChild('cardsSection') cardsSection!: ElementRef;
+ 
+  showCards = false; // Initialize as true
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  workshops_lists: any[] = [
+    { title: "UI Design and Styling ", description: "Join us and understand the principles of user experience (UX) design and learn how to create user-friendly interfaces while staying updated with the latest trends in UI design. This workshop will focus on designing with the user in mind to enhance usability and satisfaction, and provide insights into modern visual design practices, including color theory, typography, and responsive design.", src: this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/t7WsFtUGTqE?si=OMPabAqBPqphZUTX") },
+    { title: "Project Management Tools", description: "Join us as this workshop will introduce essential tools and techniques for effective collaboration. Learn about project management tools that can streamline teamwork and enhance productivity.", src:this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/TS61L2gXb68?si=bVgw6gwzJfM7DDat") },
+    { title: "Generative AI and its Applications", description: "Join us and explore the various application areas of Large Language Models (LLMs) in different sectors such as environment, commerce, health, education, and more. This comprehensive workshop will delve into real-world examples and case studies demonstrating the transformative impact of LLMs across various industries.",src: this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/gYxDZ3N7uwA?si=xgO1wkqORkLhlbdO") },
+    { title: "Pitching and Presentation Skills", description: "Learn how to craft compelling pitches that attract investment, including storytelling techniques and presentation skills. This workshop will help you refine your pitch to make a memorable impact on your audience.", src : this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/fjar3OI2HJs?si=wLFzGbgst8yhg1bE") },
+    { title: "Hackathon Dynamics", description: "Join us and learn strategies for building strong, collaborative teams and managing team dynamics during a hackathon. This workshop will cover best practices for teamwork, communication, conflict resolution, and maximizing productivity.", src : this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/prTJSU2kqzI?si=1B9sItjD-hNZbwJA")},
+    { title: "Project Idea Submission", description: "Join us and learn about the Project Idea Submission Phase and what is expected from you. This workshop is a great opportunity to learn, ask questions, and get feedback on your project ideas from experienced mentors.", src : this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/XNTJpjX8i00?si=1B9sItjD-hNZbwJA.")}
+  ];
+
+  toggleCards() {
+    this.showCards = !this.showCards; // Toggle the visibility
+    if (this.showCards) {
+      setTimeout(() => {
+        this.scrollToCardsSection();
+      }, 0);
+    }
+  }
+
+  scrollToCardsSection() {
+    console.log("scrool to section");
+    
+    this.cardsSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
+
+
--- src/app/pages/event-program/event-program.component.ts
+++ src/app/pages/event-program/event-program.component.ts
@@ -9,5 +9,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './event-program.component.css'
 })
 export class EventProgramComponent {
+  day = 1;
+  showDay() {
+    const overlay1 = document.getElementById('overlay1');
+    const overlay2 = document.getElementById('overlay2');
+    const description1 = document.getElementById("description1");
+    const description2 = document.getElementById("description2");
+
+    if (this.day === 1) {
+      overlay1?.classList.remove('opacity-0');
+      overlay1?.classList.add('opacity-90');
+      overlay2?.classList.remove('opacity-90');
+      overlay2?.classList.add('opacity-0');
+   
+      this.day = 0
+    } else {
+      overlay2?.classList.remove('opacity-0');
+      overlay2?.classList.add('opacity-90');
+      overlay1?.classList.remove('opacity-90');
+      overlay1?.classList.add('opacity-0');
+     
+      this.day = 1
+    }
+  }
 
 }
