378e28559fec44d157c2508e85553dbb57882d4a
samuel161415 <samuelnegalign1@gmail.com>
2024-09-18 13:52:52 +0300

    sami.feat : creating event program page and listing each events

--- src/app/pages/event-program/event-program.component.ts
+++ src/app/pages/event-program/event-program.component.ts
@@ -9,28 +9,76 @@ import { CommonModule } from '@angular/common';
   styleUrl: './event-program.component.css'
 })
 export class EventProgramComponent {
-  day = 1;
-  showDay() {
-    const overlay1 = document.getElementById('overlay1');
-    const overlay2 = document.getElementById('overlay2');
-    const description1 = document.getElementById("description1");
-    const description2 = document.getElementById("description2");
-
-    if (this.day === 1) {
-      overlay1?.classList.remove('opacity-0');
-      overlay1?.classList.add('opacity-90');
-      overlay2?.classList.remove('opacity-90');
-      overlay2?.classList.add('opacity-0');
-   
-      this.day = 0
-    } else {
-      overlay2?.classList.remove('opacity-0');
-      overlay2?.classList.add('opacity-90');
-      overlay1?.classList.remove('opacity-90');
-      overlay1?.classList.add('opacity-0');
-     
-      this.day = 1
+  
+   eventData = [
+    {
+        date: "Oct 11, Fri",
+        time: "",
+        title: "Workshops",
+        description: "Stage 1 and 2"
+    },
+    {
+        date: "Oct 11, Fri",
+        time: "",
+        title: "Expo Booth Setups",
+        description: "Stage 3"
+    },
+    {
+        date: "Oct 12, Sat",
+        time: "",
+        title: "Expo Day 1",
+        description: "Stage 4"
+    },
+    {
+        date: "Oct 12, Sat",
+        time: "",
+        title: "Expo Day 1 Cool Down",
+        description: "Stage 5"
+    },
+    {
+        date: "Oct 13, Sun",
+        time: "",
+        title: "Expo Day 2",
+        description: "Stage 6 and 8"
+    },
+    {
+        date: "Oct 13, Sun",
+        time: "",
+        title: "Expo Day 2 Cool Down",
+        description: "Stage 7 and 8"
+    },
+    {
+        date: "Oct 14, Mon",
+        time: "",
+        title: "Investor Panel - Inperson",
+        description: "Stage 9"
+    },
+    {
+        date: "Oct 14, Mon",
+        time: "",
+        title: "Dinner Night",
+        description: "Stage 10"
+    },
+    {
+        date: "Oct 15, Tue",
+        time: "1:00",
+        title: "Jury Evaluations for Hackathon",
+        description: "Stage 11"
+    },
+    {
+        date: "Oct 16, Wed",
+        time: "1:00, 3:00, 6:00",
+        title: "Jury Evaluations for CoreDev + Closing Ceremony",
+        description: "Stage 12 and 13"
+    },
+    {
+        date: "Oct 17, Thu",
+        time: "",
+        title: "Trip",
+        description: "Stage 14"
     }
-  }
+];
+
+
 
 }
