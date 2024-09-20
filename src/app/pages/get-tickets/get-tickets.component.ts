f5ab4182a4f32e342b5927e8e2596785bfe1659b
samuel161415 <samuelnegalign1@gmail.com>
2024-09-20 11:23:29 +0300

    sami.fix: fixing the comments on get tickets, remove images from src and reduce with compressor and use cloudinary and fix the expo presenter issue

--- src/app/pages/get-tickets/get-tickets.component.ts
+++ src/app/pages/get-tickets/get-tickets.component.ts
@@ -13,13 +13,13 @@ export class GetTicketsComponent {
     {
       title: "General",
       price: "$10",
-      features: ["Jury Pitches", "Leadership Sessions", "Closing Ceremony", "Access to All Attendees"],
+      features: ["Jury pitches", "Leadership sessions", "Closing ceremony", "In-person and Remote access"],
       ticket_link : "https://www.zeffy.com/ticketing/b44d8621-4fba-48ae-9f8b-b5c5628543eb"
     },
     {
       title: "Investor",
       price: "$25 Early Bird",
-      features: ["Investor Pitches", "Leadership Sessions", "Closing Ceremony", "Meet and Greets with Startups of choosing", "All Benefits from General Ticket"],
+      features: ["Investor pitches", "Leadership sessions", "Closing ceremony", "Meet and greets with startups of choosing", "All benefits from general ticket"],
       ticket_link : "https://www.zeffy.com/ticketing/2446451b-99a7-4305-aaee-ca44991c9d3e"
     },
     {
@@ -29,9 +29,9 @@ export class GetTicketsComponent {
       ticket_link : "https://www.zeffy.com/ticketing/b321a500-a560-4016-858c-71bb4bd686e7"
     },
     {
-      title: "Student (virtual)",
+      title: "Student",
       price: "Free",
-      features: ["Access to YouTube link for livestream of October 14th: Workshops"],
+      features: ["Access to YouTube link for livestream of October 14th and 15th: Workshops","Remote access"],
       ticket_link : "https://www.zeffy.com/ticketing/909dd2ea-8bfd-4c36-8f7e-dfbd07a659c8"
     }
    
